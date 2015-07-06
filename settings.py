fd3cdd9f14a0d983c7f36491bb935bebdbdf1e0d
edmondliang <edmond.liang.sf@gmail.com>
2015-07-05 22:52:28 -0700

    feat: basic function

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '585214976686-uvp5r3ulipj9213d6pibh0dd4u39b1ck.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
--- static/js/app.js
+++ static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '585214976686-uvp5r3ulipj9213d6pibh0dd4u39b1ck.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
